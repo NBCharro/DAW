95ae02bc01d8407ea7b2b4353b1967398dc57be7
Nielsen White <nbcharro@gmail.com>
2021-12-02 10:18:27 +0000

    Agregadas primeras tareas

---
+++ Programacion/PROG01_programa1.java
@@ -0,0 +1,27 @@
+ /**
+ @author Nelson Blanco Charro
+
+El programa PROG_programa1 implementa una aplicación que muestra por pantalla:
+
+Modulo Profesional - PROGRAMACION
+UNIDAD DE TRABAJO 01
+Introduccion a la programacion
+nombre y apellidos del alumno/a
+Localidad y provincia
+Fecha de realizacion del ejercicio
+programa1
+*/
+
+class PROG01_programa1 {
+
+    public static void main(String args[]) {
+    System.out.println("Modulo Profesional - PROGRAMACION");
+    System.out.println("UNIDAD DE TRABAJO 01");
+    System.out.println("Introduccion a la programacion");
+    System.out.println("Nelson Blanco Charro");
+    System.out.println("Cuevas del Valle - Avila");
+    System.out.println("22-10-2021");
+    System.out.println("programa1");
+    }
+
+} 
---
+++ Programacion/programa1.java
@@ -0,0 +1,40 @@
+public class programa1 {
+ public static void main(String[] args) { 
+	boolean casado = true;
+	final int MAXIMO = 999999;
+	byte diasem = 1;
+	short diaanual = 300;
+	long miliseg = 1298332800000L;
+	char sexo = 'M';
+	double totalFactura = 10350.678;
+	long poblacion = 6775235741L;
+	System.out.println("El valor de la variable casado es " + casado);
+	System.out.println("El valor de la variable MAXIMO es " + MAXIMO);
+	System.out.println("El valor de la variable diasem es " + diasem);
+	System.out.println("El valor de la variable diaanual es " + diaanual);
+	System.out.println("El valor de la variable miliseg es " + miliseg);
+	System.out.println("El valor de la variable totalfactura es " + totalFactura);
+	System.out.println("El valor de la variable poblacion es " + poblacion);
+	System.out.println("El valor de la variable sexo es " + sexo);
+	System.out.println("");
+	System.out.print("El valor de la variable casado es " + casado);
+	System.out.print("El valor de la variable MAXIMO es " + MAXIMO);
+	System.out.print("El valor de la variable diasem es " + diasem);
+	System.out.print("El valor de la variable diaanual es " + diaanual);
+	System.out.print("El valor de la variable miliseg es " + miliseg);
+	System.out.print("El valor de la variable totalfactura es " + totalFactura);
+	System.out.print("El valor de la variable poblacion es " + poblacion);
+	System.out.print("El valor de la variable sexo es " + sexo);
+	System.out.println("");
+	System.out.printf("El valor de la variable casado es " + casado);
+	System.out.printf("El valor de la variable MAXIMO es " + MAXIMO);
+	System.out.printf("El valor de la variable diasem es " + diasem);
+	System.out.printf("El valor de la variable diaanual es " + diaanual);
+	System.out.printf("El valor de la variable miliseg es " + miliseg);
+	System.out.printf("El valor de la variable totalfactura es " + totalFactura);
+	System.out.printf("El valor de la variable poblacion es " + poblacion);
+	System.out.printf("El valor de la variable sexo es " + sexo);
+ } 
+
+} 
+
